3b61e3dff2e2bfe792da45e975764dc280f20e3b
Daigo Kijima <norimaking777@gmail.com>
2026-01-22 21:33:29 +0900

    docs: ドキュメントに階層をつけて整理した

--- internal/cmd/generate_item_doc.go
+++ internal/cmd/generate_item_doc.go
@@ -31,7 +31,7 @@ func runGenerateItemDoc(_ context.Context, _ *cli.Command) error {
 	}
 
 	// Markdownファイルを開く
-	file, err := os.Create("docs/item_tables.md")
+	file, err := os.Create("docs/gen/item_tables.md")
 	if err != nil {
 		return fmt.Errorf("error creating file: %w", err)
 	}
